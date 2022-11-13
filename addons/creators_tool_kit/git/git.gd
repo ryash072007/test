fa08d067a9e03fb6c2bc746b19fdf3774b7db714
Ryash <ryash072007@gmail.com>
2022-11-13 19:18:15 +0400

    2022-11-13 at time 19:18:15

--- addons/creators_tool_kit/git/git.gd
+++ addons/creators_tool_kit/git/git.gd
@@ -9,20 +9,22 @@ func _on_ini_pressed():
 
 func _on_push_pressed():
 	var output: Array
-	OS.execute("git", ["remote", "add", "origin", $VBoxContainer/HBoxContainer/repo.text], output, true, true)
+	OS.execute("git", ["remote", "add", "origin", $VBoxContainer/HBoxContainer/repo.text], output, true, false)
 	var output_string: String = output[0]
-	
+	print(output_string)
 	
 	output = []
-	OS.execute("git", ["add", "*"], output, true, true)
+	OS.execute("git", ["add", "*"], output, true, false)
 	output_string = output[0]
+	print(output_string)
 	
-	
-	var message := Time.get_datetime_string_from_system()
+	var message := Time.get_datetime_string_from_system().replace("T", " at time ")
 	if $VBoxContainer/message.text != "": message = $VBoxContainer/message.text
 
 	output = []
-	OS.execute("git", ["commit", "-m", message], output, true, true)
+	OS.execute("git", ["commit", "-m", message], output, true, false)
 	output_string = output[0]
+	print(output_string)
 	
+	print("Pushing to GitHub...")
 	OS.create_process("git", ["push", "origin", "master"], true)
